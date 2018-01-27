3ab084ab86732135c0b6fe41539601adb7df7ef7
Wai <12164553+Wai-Lau@users.noreply.github.com>
2018-01-27 16:11:06 -0500

    added webhook route

--- request.py
+++ request.py
@@ -1,12 +1,22 @@
 import json
-import md5
 import random
 import datetime
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, abort
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     return render_template("main.html", message="")
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        print(request.json)
+        return '', 200
+    else:
+        abort(400)
+
+
+if __name__ == '__main__':
+    app.run()
